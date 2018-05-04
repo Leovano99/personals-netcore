b5765f262087253449ba942ce347140e2887b166
Claudia <Claudia@C>
2018-05-04 11:02:20 +0700

    Menambah Service Dropdown City

---
+++ src/VDI.Demo.Application.Shared/Personals/MS_City/Dto/GetCityListDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VDI.Demo.Personals.MS_City.Dto
+{
+    public class GetCityListDto
+    {
+        public string cityCode { get; set; }
+        public string cityName { get; set; }
+    }
+}
---
+++ src/VDI.Demo.Application.Shared/Personals/MS_City/IMsCityAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Application.Services;
+using VDI.Demo.Personals.MS_City.Dto;
+
+namespace VDI.Demo.Personals.MS_City
+{
+    public interface IMsCityAppService : IApplicationService
+    {
+        List<GetCityListDto> GetCityListByProvinceCode(string provinceCode);
+    }
+}
