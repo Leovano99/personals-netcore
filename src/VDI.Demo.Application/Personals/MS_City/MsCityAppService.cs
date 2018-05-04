ab0fcaefb898c967706ca85bf26120633f55ca47
Claudia <Claudia@C>
2018-05-04 11:12:42 +0700

    CItyAppService

--- src/VDI.Demo.Application/Personals/MS_City/MsCityAppService.cs
+++ src/VDI.Demo.Application/Personals/MS_City/MsCityAppService.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using Abp.Domain.Repositories;
 using VDI.Demo.Personals.MS_City.Dto;
-using VDI.Demo.PersonalsDB;
 
 namespace VDI.Demo.Personals.MS_City
 {
     public class MsCityAppService : DemoAppServiceBase, IMsCityAppService
     {
-        //private readonly IRepository<MS_City> _msCityRepo;
+        private readonly IRepository<MS_City> _msCityRepo;
 
-        //public MsCityAppService(IRepository<MS_City, string> msCityRepo)
-        //{
-        //    msCityRepo = _msCityRepo;
-        //}
+        public MsCityAppService(IRepository<MS_City> msCityRepo)
+        {
+            _msCityRepo = msCityRepo;
+        }
 
-
-        //public List<GetCityListDto> GetCityListByProvinceCode(string provinceCode)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public List<GetCityListDto> GetCityListByProvinceCode(string provinceCode)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
